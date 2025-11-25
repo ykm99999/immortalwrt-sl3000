ca7a0526e8b14afdf653cf64c571f77f98894d64
ykm888 <165190180+ykm888@users.noreply.github.com>
2025-11-25 23:36:42 +0800

    Create mt7981.mk

---
+++ target/linux/mediatek/image/mt7981.mk
@@ -0,0 +1,23 @@
+define Device/siluo_sl3000
+  DEVICE_VENDOR := Siluo
+  DEVICE_MODEL := SL-3000
+  DEVICE_DTS := mt7981-siluo-sl3000
+  DEVICE_PACKAGES := \
+    kmod-mt7981-wifi \
+    uboot-envtools \
+    luci \
+    luci-i18n-base-zh-cn \
+    luci-theme-bootstrap \
+    luci-app-firewall \
+    luci-app-upnp \
+    luci-app-samba4 \
+    luci-app-ddns \
+    luci-app-docker \
+    luci-app-passwall2 \
+    luci-i18n-passwall2-zh-cn \
+    luci-app-ssr-plus \
+    luci-i18n-ssr-plus-zh-cn \
+    luci-app-turboacc \
+    luci-i18n-turboacc-zh-cn
+endef
+TARGET_DEVICES += siluo_sl3000
