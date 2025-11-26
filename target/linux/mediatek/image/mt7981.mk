825ac8c313fa79bcd51e0b3cabf34c635c1c0fd7
ykm888 <165190180+ykm888@users.noreply.github.com>
2025-11-26 20:22:35 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -1,7 +1,10 @@
 define Device/siluo_sl3000
   DEVICE_VENDOR := Siluo
-  DEVICE_MODEL := SL-3000
+  DEVICE_MODEL := SL3000
   DEVICE_DTS := mt7981-siluo-sl3000
-  DEVICE_PACKAGES := kmod-mt7981-wifi kmod-mmc uboot-envtools
+  DEVICE_PACKAGES := kmod-mt7981-wifi wpad-openssl luci
+  IMAGES := sysupgrade.bin initramfs.bin
+  IMAGE/sysupgrade.bin := append-rootfs | gzip | append-metadata
+  IMAGE/initramfs.bin := append-initramfs | gzip
 endef
 TARGET_DEVICES += siluo_sl3000
